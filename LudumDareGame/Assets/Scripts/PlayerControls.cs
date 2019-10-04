5520245f99714dae2b37e732e556603d9b8294e4
GustavoMaranhao <GustavoMaranhao@users.noreply.github.com>
2019-10-04 20:18:15 -0300

    Initial Commit

---
+++ LudumDareGame/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+
+        }
+    }
+}
